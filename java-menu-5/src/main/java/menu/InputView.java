98274539332d91a56ccf88ec9203ec67e9eca03b
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2024-12-12 13:59:51 +0900

    feat : 코치 이름 입력 받는다.

--- java-menu-5/src/main/java/menu/InputView.java
+++ java-menu-5/src/main/java/menu/InputView.java
@@ -1,5 +1,17 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    private static final String INPUT_COACH_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+
+    public String inputCoach() {
+        println(INPUT_COACH_MESSAGE);
+        return Console.readLine();
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
 }
