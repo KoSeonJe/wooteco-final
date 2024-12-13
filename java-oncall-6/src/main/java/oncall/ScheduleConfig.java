08f863fec08432daaa1d92aef594caeea6c2d40d
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2024-12-13 16:36:54 +0900

    fix : 의존성 주입

--- java-oncall-6/src/main/java/oncall/ScheduleConfig.java
+++ java-oncall-6/src/main/java/oncall/ScheduleConfig.java
@@ -3,6 +3,26 @@ package oncall;
 public class ScheduleConfig {
 
     public WorkScheduler workScheduler() {
-        return new WorkScheduler();
+        return new WorkScheduler(applicationView(), workService());
+    }
+
+    private ApplicationView applicationView() {
+        return new ApplicationView(new InputView(), new OutputView(), new WorkerMapper());
+    }
+
+    private WorkService workService() {
+        return new WorkService(workRepository(), weekWorkerRepository(), holiWorkerRepository());
+    }
+
+    private WorkRepository workRepository() {
+        return WorkRepository.getInstance();
+    }
+
+    private WeekWorkerRepository weekWorkerRepository() {
+        return WeekWorkerRepository.getInstance();
+    }
+
+    private HoliWorkerRepository holiWorkerRepository() {
+        return HoliWorkerRepository.getInstance();
     }
 }
